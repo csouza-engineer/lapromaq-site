c6e8a667c25368761287ee0a30aeb9b0af4404c9
Cristiano de Souza <csouza@ufgd.edu.br>
2026-04-02 09:22:23 -0400

    Atualiza

---
+++ 1cnf/outline.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Cristiano Márcio
+vti_modifiedby:SR|Cristiano Márcio
+vti_timecreated:TR|27 Jan 2013 11:39:52 -0000
+vti_timelastmodified:TR|27 Jan 2013 11:39:52 -0000
+vti_cacheddtm:TX|27 Jan 2013 11:39:52 -0000
+vti_filesize:IR|3143
+vti_extenderversion:SR|4.0.2.4426
+vti_backlinkinfo:VX|armazena.html secaera.html biomassa.html otimizoo.html po.html
